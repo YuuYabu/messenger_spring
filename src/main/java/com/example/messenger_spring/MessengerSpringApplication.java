f5b7347bbbe7d067c47535090c5807d0be608e14
Yuu YABU <wongty0220@gmail.com>
2023-03-28 16:55:01 +0900

    first commit

---
+++ src/main/java/com/example/messenger_spring/MessengerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.messenger_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/messenger_spring/MessengerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messenger_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
